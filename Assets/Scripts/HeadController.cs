b60292f9f1723ceb5c0b66d9edbe6dd8e6c289bc
jamesllg <39319412+jamesllg@users.noreply.github.com>
2018-05-16 10:33:02 +1000

    add files via upload (base)

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+
+    public int maxRotationHeight;
+    public int minRotationHeight;
+    public float euX;
+    public float nextVertical;
+    public float rotationSpeed;
+    public float lagRotationSpeed;
+    public bool goodTouch = false;
+    public bool lagFriendly = false;
+
+    private Vector2 touchOrigin = -Vector2.one;
+    private Vector2 touchCurrent = -Vector2.one;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Rotate head around local x-axis from touch input
+        float vertical = 0;
+        if (Input.touchCount > 0)
+        {
+            Touch touch1 = Input.touches[0];
+            if (touch1.phase == TouchPhase.Began && touch1.position.y > Screen.height / 3)
+            {
+                goodTouch = true;
+                touchOrigin = touch1.position;
+            }
+
+            if (touch1.phase == TouchPhase.Moved && touch1.position.y > Screen.height / 3 && goodTouch)
+            {
+                touchCurrent = touch1.position;
+                if (lagFriendly)
+                {
+                    vertical = lagRotationSpeed * (touchCurrent.y - touchOrigin.y) * Time.deltaTime / Screen.height;
+
+                }
+                else
+                {
+                    vertical = rotationSpeed * (touchCurrent.y - touchOrigin.y) / Screen.height;
+
+                }
+                euX = transform.eulerAngles.x;
+                nextVertical = (vertical + transform.eulerAngles.x) % 360;
+                if (nextVertical < maxRotationHeight ||
+                    nextVertical > minRotationHeight)
+                {
+                    transform.Rotate(vertical, 0.0f, 0.0f);
+                }
+            }
+
+            if (touch1.phase == TouchPhase.Ended)
+            {
+                goodTouch = false;
+            }
+
+            touchOrigin = touch1.position;
+        }
+    }
+}
---
+++ Assets/Scripts/InformationText.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationText : MonoBehaviour
+{
+    public float lifetime;
+    private Text t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponentInChildren<Text>();
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetText(string text)
+    {
+        
+        gameObject.SetActive(true);
+        t.text = text;
+        StopAllCoroutines();
+        StartCoroutine(TextAppear());
+    }
+
+    IEnumerator TextAppear()
+    {
+
+        yield return new WaitForSeconds(lifetime);
+        gameObject.SetActive(false);
+    }
+}
